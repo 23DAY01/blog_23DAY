fcf86b340e41edcf5b3162e65284f79ac83a2bfa
23DAY <1784247914@qq.com>
2023-01-17 11:40:31 +0800

    init

---
+++ src/main/java/site/day/blog/Blog23DayApplication.java
@@ -0,0 +1,13 @@
+package site.day.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Blog23DayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Blog23DayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/site/day/blog/Blog23DayApplicationTests.java
@@ -0,0 +1,13 @@
+package site.day.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Blog23DayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
